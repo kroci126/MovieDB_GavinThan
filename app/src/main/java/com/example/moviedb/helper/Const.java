81040496adbd34ea1d955005486170908d5fe156
kroci126 <gavinthan01@student.ciputra.ac.id>
2021-10-18 00:47:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviedb/helper/Const.java
@@ -0,0 +1,9 @@
+package com.example.moviedb.helper;
+
+public class Const {
+
+    public static final String API_KEY = "396376b4b56ae9aeed9e387b65ff9def";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+}
---
+++ app/src/main/java/com/example/moviedb/view/MovieDetailsActivity.java
@@ -0,0 +1,67 @@
+package com.example.moviedb.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.moviedb.R;
+import com.example.moviedb.adapter.NowPlayingAdapter;
+import com.example.moviedb.helper.Const;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    private String movie_id = "";
+    private String date = "";
+    private String title = "";
+    private String overview = "";
+    private String img_path = "";
+    private String popularity = "";
+
+    TextView lbl_title, lbl_overview, lbl_date, buatdate, buatpopularity, lbl_popularity;
+    ImageView movie_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        Intent intent = getIntent();
+        movie_id = intent.getStringExtra("movie_id");
+        date = intent.getStringExtra("movie_rdate");
+        title = intent.getStringExtra("movie_title");
+        overview = intent.getStringExtra("movie_overview");
+        img_path = intent.getStringExtra("movie_poster");
+        popularity = intent.getStringExtra("movie_popularity");
+
+        lbl_title = findViewById(R.id.lbl_details_title);
+        lbl_title.setText(title);
+
+        lbl_overview = findViewById(R.id.lbl_details_overview);
+        lbl_overview.setText(overview);
+
+        movie_image = findViewById(R.id.img_details_poster);
+        Glide.with(MovieDetailsActivity.this).load(img_path).into(movie_image);
+
+        lbl_date = findViewById(R.id.lbl_details_date);
+        lbl_date.setText(date);
+
+        lbl_popularity = findViewById(R.id.lbl_details_popularity);
+        lbl_popularity.setText(popularity);
+
+        buatdate = findViewById(R.id.release_date);
+        buatdate.setText("Release date: ");
+
+        buatpopularity = findViewById(R.id.popularity_bar);
+        buatpopularity.setText("Popularity: ");
+
+
+
+//        lbl_text = findViewById(R.id.lbl_movie_details);
+//        lbl_text.setText(movie_id);
+
+    }
+}
